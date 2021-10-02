3e2eff09be43e84b9fa69877d97ff29652f80c18
Grzegorz Sirocki <daddyip@wp.pl>
2021-10-02 18:57:03 +0200

    ćwiczenie pierwsze

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Pochmurnie.");
+        System.out.println("Nadchodzi zima.");
+
+        System.out.printf("dzisiaj %s kupimy %d lody.\n", "sobie", 2 );
+        System.out.printf("Lewandowski %s dziś %d bramki.\n", "strzelił", 3 );
+
+        System.out.print("Robimy dziś");
+        System.out.print(" tort urodzinowy");
+        System.out.print(" dla mamy");
 
 
     }
