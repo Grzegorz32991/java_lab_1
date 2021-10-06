e91cbbcb2f7303522665189170f1722967f410f6
Grzegorz Sirocki <daddyip@wp.pl>
2021-10-06 20:37:44 +0200

    ćwiczenie trzecie

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,40 @@ public class Main {
         *   wynik wyświetlić w konsoli programu
         */
 
+
+        //zad1
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj Imię: ");
+        String Imie = scan.next();
+
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("Podaj wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.print("Podaj numer indeksu: ");
+        int nr_indeksu = scan.nextInt();
+
+        System.out.println(" Imię:" + Imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek + " Nr indeksu: " + nr_indeksu +"");
+        System.out.printf("Imię: %s Nazwisko: %s Wiek: %d Nr indeksu %d \n", Imie, nazwisko, wiek, nr_indeksu);
+
+
+
+        //zad2
+        int zmiennapierwsza = 9;
+        int zmiennadruga = 7;
+        int wynik_dodawania = zmiennapierwsza + zmiennadruga;
+        int wynik_odejmowania = zmiennapierwsza - zmiennadruga;
+        int wynik_mnozenia = zmiennapierwsza * zmiennadruga;
+        int wynik_dzielenia = zmiennapierwsza / zmiennadruga;
+        int wynik_modulo = zmiennapierwsza % zmiennadruga;
+
+        System.out.println("\n wynik dodawania: " + wynik_dodawania+ "\n wynik odejmowania: " + wynik_odejmowania + "\n wynik mnożenia: " + wynik_mnozenia + "\n wynik dzielenia: " + wynik_dzielenia + "\n wynik modulo: " + wynik_modulo);
+
+
+
     }
 }
