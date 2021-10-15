e7b05fa2c6c3f9b0dfe75cfffc05620f7cd6d599
Grzegorz Sirocki <daddyip@wp.pl>
2021-10-15 19:39:29 +0200

    ćwiczenie piąte

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,34 @@ public class Main {
         /* zad.1 podziel swój wiek przez modulo 3 i jeśli twój wiek zwraca 0 wyświetl napis "Podzielny przez 3",
          * w innym wypadku "Niepodzielny przez 3" użyj kontrukcji if else
          * */
+         int wiek = 23 % 3;
+        System.out.println(wiek); // 2
+        if (wiek == 0) {
+            System.out.println("podzielny przez 3");
+        } else {
+            System.out.println("Niepodzielny przez 3");
+        }
+
+
 
 
         /* zad.2 użyj parametru trójargumentowego aby sprawdzić parzystośc nr. indeksu */
 
+        int numer_albumu = 57089;
+        int parzystosc = (numer_albumu % 2 == 0) ? 0 : 1;
+        System.out.println("Parzystość: " +parzystosc);
 
         /* zad.3 wpisz z klawiatury liczbę zmiennoprzecinkową (użyj klasy Scanner z poprzednich ćwiczeń)
          * i stwórz konstrukcję else if z dowolnymi komunikatami */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoją wagę:");
+        double first = scanner.nextDouble();
+        System.out.println("Podaj swoj wzrost:");
+        double second = scanner.nextDouble();
+        double sum = first + second;
+
+        System.out.println("Suma wprowadzonych liczb to " + sum);
 
     }
 }
