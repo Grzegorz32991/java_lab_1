0d6a6d91ed2abdc33998c253a1f08c562f2b7e2b
Grzegorz Sirocki <daddyip@wp.pl>
2021-10-15 22:00:03 +0200

    ćwiczenie szóste

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[] podatek = new double[5];
+        podatek[0] = 154.25;
+        podatek[1] = 558.91;
+        podatek[2] = 1254.65;
+        podatek[3] = 987.12;
+        podatek[4] = 87.12;
+        for(int i=0; i < podatek.length; i++) {
+            System.out.println(podatek[i]);
+        }
     }
 }
