9ae2e2b6e2a9fd890c064fee0e31b8b55934af54
Grzegorz Sirocki <daddyip@wp.pl>
2021-10-15 18:48:36 +0200

    ćwiczenie czwarte

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,40 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        int a = 9;
+        int b = 3;
+        int c = 3;
+        int d = a + b + c;
+        System.out.println(d);
+        d = a - b + c;
+        System.out.println(d);
+        d= a / b * c;
+        System.out.println(d);
+        d= a * c + b;
+        System.out.println(d);
+        d = a % c + b;
+        System.out.println(d);
+
+
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x && y = " + (x&&y));
+        System.out.println("x || y = " + (x||y) );
+        System.out.println("!(x && y) = " + !(x && y));
+        System.out.println("((x && y) || (!x || y) : " + ((x && y) || (!x || y)));
+        System.out.println("!((x || y) || (x || !y) : " + !((x && y) || (!x || y)));
+        System.out.println("!((x && y) || (!x || y) : " + !((x && y) || (!x || y)));
+        System.out.println("((x && y) %% (x || y) : " + ((x && y) && (x || y)));
+
+
+
+
+
+
+
+
+
+
+
     }
 }
